94cf6275ae36ab84341d9bd7c03676b33d40bc16
Vincent Jousse <vincent.jousse@knplabs.com>
2011-02-09 15:32:23 +0100

    Enhanced logger

--- server.js
+++ server.js
@@ -110,10 +110,14 @@ socket.on('connection', function(client){
 });
 
 
-process.on('uncaughtException', function (err) {
-    //We should certainly do something here :)
-    console.log(err);
+process.addListener('uncaughtException', function (err, stack) {
+  console.log('------------------------');
+  console.log('Exception: ' + err);
+  console.log(err.stack);
+  console.log('------------------------');
 });
+
+
 
 
 app.listen(3000);
