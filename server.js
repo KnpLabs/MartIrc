fe3c3df1adf16409b98e9a1b0b41cb1cfb17bfcd
Vincent Jousse <vincent.jousse@knplabs.com>
2011-02-09 15:46:05 +0100

    Moved application logic to lib/martirc.js

--- server.js
+++ server.js
@@ -3,112 +3,9 @@ var path = require('path');
 require.paths.unshift( path.join( __dirname,'vendor') )
 require.paths.unshift( path.join( __dirname,'vendor','coloured-log','lib') )
 
-var app = require('express/lib/express').createServer(),
-    irc = require('IRC-js/lib/irc'),
-    io = require('Socket.IO-node'),
-    socket = io.listen(app);
+var MartIrc = require('./lib/martirc');
 
-var opts = {
-    encoding: "utf-8",
-};
-var webClients = [];
-var servers = [];
-
-socket.on('connection', function(client){
-
-    client.on('message', function(clientMessage){
-        console.log("got a message :: "+ clientMessage.type);
-
-        switch(clientMessage.type)
-        {
-            case 'connect':
-
-                var server = new irc({
-                    server: clientMessage.data.ircHost,
-                    port: clientMessage.data.ircPort,
-                    nick: clientMessage.data.nick,
-                    encoding: opts.encoding
-                });
-
-                channels = clientMessage.data.channels;
-
-                server.connect(function() {
-                    setTimeout(function() {
-                        for(i in channels) {
-                            server.join(channels[i]);
-                        }
-                    }, 2000);
-                });
-
-                server.addListener('privmsg', function(msg) {
-                    nick = msg.person.nick;
-                    chan = msg.params[0];
-                    message = msg.params[1];
-
-                    var data = {channel: chan, from:nick, msg:message};
-
-                    console.log("IRC new: "+msg.params[0]+" - "+msg.person.nick+":"+msg.params[1]+"\n");
-
-
-                    if(webClients.length != 0) {
-                        for(i in webClients) {
-                            webClient = webClients[i];
-
-                            if(webClient.server == this) {
-
-                                for(j in webClient.channels) {
-
-                                    if(chan.toLowerCase() == webClient.channels[j].toLowerCase()) {
-
-                                        webClient.client.send(data);
-
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-
-                });
-
-                webClients.push({session:client.sessionId,client:client, server:server, channels: channels});
-                console.log("got a client :: "+client.sessionId+" :: "+webClients.length);
-
-                client.send({msgs:[],channels: channels});
-
-                break;
-
-            case 'privmsg':
-                console.log("got a message for " + clientMessage.data.channel + " :: "+ clientMessage.data.message);
-
-                if(webClients.length != 0) {
-                    for(i in webClients) {
-                        webClient = webClients[i];
-
-                        if(webClient.session == client.sessionId) {
-                            //TODO: change the from nickname
-                            webClient.client.send({channel: clientMessage.data.channel, from:'me', msg:clientMessage.data.message});
-                            webClient.server.privmsg(clientMessage.data.channel, clientMessage.data.message);
-                        }
-                    }
-                }
-
-                break;
-
-        }
-    });
-
-    client.on('disconnect', function(){ 
-        for(i in webClients) {
-            if(webClients[i].session == client.sessionId) {
-                webClients[i].server.quit("Leaving");
-                webClients.splice(i,1);
-            }
-        }
-        console.log("disconnect");
-    });
-});
-
+new MartIrc({ port: 3000 });
 
 process.addListener('uncaughtException', function (err, stack) {
   console.log('------------------------');
@@ -117,7 +14,3 @@ process.addListener('uncaughtException', function (err, stack) {
   console.log('------------------------');
 });
 
-
-
-
-app.listen(3000);
