4f42819e6f33a06d03a582cdf47d31663ea512f5
Vincent Jousse <vincent.jousse@knplabs.com>
2011-02-20 16:36:09 +0100

    Added PSDoc

---
+++ doc/javascripts/pdoc/item_index.js
@@ -0,0 +1,13 @@
+if (!window.PDoc) window.PDoc = {};
+PDoc.elements = {
+  'MartIrc': { 'name': 'MartIrc', 'type': 'class', 'path': 'MartIrc/index.html' },
+'new MartIrc': { 'name': 'new MartIrc', 'type': 'constructor', 'path': 'MartIrc/new/index.html' },
+'MartIrc#init': { 'name': 'MartIrc#init', 'type': 'instance method', 'path': 'MartIrc/prototype/init/index.html' },
+'MartIrc#createHTTPServer': { 'name': 'MartIrc#createHTTPServer', 'type': 'instance method', 'path': 'MartIrc/prototype/createHTTPServer/index.html' },
+'MartIrc#createIrcServerForClient': { 'name': 'MartIrc#createIrcServerForClient', 'type': 'instance method', 'path': 'MartIrc/prototype/createIrcServerForClient/index.html' },
+'MartIrc#disconnectClient': { 'name': 'MartIrc#disconnectClient', 'type': 'instance method', 'path': 'MartIrc/prototype/disconnectClient/index.html' },
+'MartIrc#sendMessageToServerChannelFromClient': { 'name': 'MartIrc#sendMessageToServerChannelFromClient', 'type': 'instance method', 'path': 'MartIrc/prototype/sendMessageToServerChannelFromClient/index.html' },
+'MartIrc#removeClientById': { 'name': 'MartIrc#removeClientById', 'type': 'instance method', 'path': 'MartIrc/prototype/removeClientById/index.html' },
+'MartIrc#findClientById': { 'name': 'MartIrc#findClientById', 'type': 'instance method', 'path': 'MartIrc/prototype/findClientById/index.html' },
+'MartIrc#sendMessageFromServerChannelToClient': { 'name': 'MartIrc#sendMessageFromServerChannelToClient', 'type': 'instance method', 'path': 'MartIrc/prototype/sendMessageFromServerChannelToClient/index.html' }
+};
