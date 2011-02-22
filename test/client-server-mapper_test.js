70707510df698168089318c8acfa34ba51d0e42d
Vincent Jousse <vincent.jousse@knplabs.com>
2011-02-22 16:32:10 +0100

    Finished simple Client/Server mapper

--- test/client-server-mapper_test.js
+++ test/client-server-mapper_test.js
@@ -8,19 +8,49 @@ var ClientServerMapper = require( 'client-server-mapper' );
 
 /* ------------------------------ Fixtures ------------------------------ */
 
-var client = {
-        , name: 'some client name'
+var clientId = 1;
+var serverId = 1;
+
+function getClientWithoutUuid() {
+
+    var client = {
+        name: 'some client name'
         , description: 'some client description'
-};
+    };
+
+    return client;
+
+}
 
 
-var server = {
-        , name: 'some server name'
+function getClient() {
+    var client = getClientWithoutUuid();
+    client.uuid = clientId++;
+
+    return client;
+}
+
+function getServerWithoutUuid() {
+    var server = {
+        name: 'some server name'
         , description: 'some server description'
-};
+    };
 
+    return server;
+}
+
+
+function getServer() {
+    var server = getServerWithoutUuid();
+    server.uuid = serverId++;
+
+    return server;
+}
 exports['uuid exception'] = function(test) {
     var mapper = new ClientServerMapper();
+
+    client = getClientWithoutUuid();
+    server = getServerWithoutUuid();
 
     test.throws(
             function() {
@@ -28,6 +58,39 @@ exports['uuid exception'] = function(test) {
             },
             '/uuid/'
     );
+
+    test.finish();
+};
+
+
+exports['map'] = function(test) {
+    var mapper = new ClientServerMapper();
+
+    var client = getClient();
+    var server = getServer();
+
+    test.doesNotThrow(
+            function() {
+                mapper.map(client, server);
+            },
+            '/uuid/'
+    );
+
+    clientMapped = mapper.getClientForServer(server);
+    test.equal(clientMapped.uuid,client.uuid);
+
+    serverMapped = mapper.getServerForClient(client);
+    test.equal(serverMapped.uuid,server.uuid);
+
+    var newClient = getClient();
+
+    test.throws(
+            function() {
+                mapper.map(newClient, server);
+            },
+            '/exists/'
+    );
+
 
     test.finish();
 };
