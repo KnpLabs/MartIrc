1e8d0dbd0ee71eaf4a01d6b6c1fedc3ca23d4fdd
Anthony HAMON <hamonanthony19@gmail.com>
2011-04-01 18:30:06 +0200

    Add channels and users tabs

--- static/js/interface.js
+++ static/js/interface.js
@@ -2,6 +2,12 @@ $('document').ready(function(){
 			$('.prompt form').submit(sendMsg);
 
 			$('.prompt form input').focus();
+
+			$('.channels div a.channel').live('click', focusOnChannel);
+
+			$('.channels div a.user').live('click', focusOnUser);
+
+			$('.users .list.active a').live('click', focusOnUser);
 		    });
 
 function sendMsg()
@@ -10,11 +16,96 @@ function sendMsg()
 
     $('.prompt form input').val('');
 
-    $('.chat div').append('<span class="msg"><span class="current-user nick">Mickael : </span>'+'<span class="txt">'+prompt+'</span></span>');
+    $('.chat .active').append('<span class="msg"><span class="current-user nick">Mickael : </span>'+'<span class="txt">'+prompt+'</span></span>');
 
-    $(".chat div").attr({ scrollTop: $(".chat div").attr("scrollHeight") });
-
-    $('.prompt form input').focus();
+    focusOnPrompt();
 
     return false;
+}
+
+function focusOnPrompt()
+{
+    $(".chat .active").attr({ scrollTop: $(".chat .active").attr("scrollHeight") });
+
+    $('.prompt form input').focus();
+}
+
+function displayUsersTab(display){
+
+    if(display){
+	$('.chat, .prompt').removeClass('span-19');
+	$('.chat, .prompt').addClass('span-16');
+	$('.prompt').removeClass('append-1');
+	$('.prompt').addClass('append-4');
+	$('.prompt .text').addClass('span-14');
+	$('.prompt .text').removeClass('span-17');
+	$('.users').show();
+    } else {
+	$('.chat, .prompt').removeClass('span-16');
+	$('.chat, .prompt').addClass('span-19');
+	$('.prompt').removeClass('append-4');
+	$('.prompt').addClass('append-1');
+	$('.prompt .text').removeClass('span-14');
+	$('.prompt .text').addClass('span-17');
+	$('.users').hide();
+    }
+}
+
+function focusOnChannel()
+{
+    var self = $(this);
+
+    var id = self.attr('id');
+    var channelToDisable = $('.channels .active');
+    var chatAndUsersToDisable = $('.chat .active, .users .list.active');
+
+    displayUsersTab(true);
+
+    chatAndUsersToDisable.hide();
+    channelToDisable.removeClass('active');
+    chatAndUsersToDisable.removeClass('active');
+
+    var chatAndUsersToEnable = $('.chat .'+id+', .users .list.'+id).addClass('active');
+    chatAndUsersToEnable.show();
+
+    $('#'+id).addClass('active');
+
+    focusOnPrompt();
+}
+
+function focusOnUser()
+{
+    var self = $(this);
+
+    var id;
+
+    if(self.attr('id')){
+	id = self.attr('id');
+    } else {
+	id = self.attr('class');
+    }
+
+    var channelToDisable = $('.channels .active');
+    var chatAndUsersToDisable = $('.chat .active, .users .list.active');
+
+    displayUsersTab(false);
+
+    if(!$('.channels div a#'+id).get(0)){
+	var user = $('<a>').attr('id', id).attr('class', 'user').text(self.text());
+	$('.channels div').append(user);
+
+	var userChat = $('<div>').attr('class', id);
+	$('.chat').append(userChat);
+    }
+
+    chatAndUsersToDisable.hide();
+    channelToDisable.removeClass('active');
+    chatAndUsersToDisable.removeClass('active');
+
+    var chatToEnable = $('.chat .'+id).addClass('active');
+    chatToEnable.show();
+
+    $('#'+id).addClass('active');
+
+    focusOnPrompt();
 }
