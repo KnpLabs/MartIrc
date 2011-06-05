5517af5c8c55d075aa45083337a1ef9c91a40001
Vincent Velociter <vincent.velociter@gmail.com>
2011-06-05 21:11:31 +0200

    Work on an implementation of dynamic height
    For now it does the job. It definitely can be done in a better way with
    just one calculation in JS and the rest in CSS. I could not find a way
    to do it with the present layout.

--- client/js/init.js
+++ client/js/init.js
@@ -7,4 +7,25 @@ $(document).ready(function() {
 
     $("#userPreferencesLink[rel]").overlay();
 
+    function computeChatWrapperHeight() {
+        return $(window).height() - $('#header').height() - $('#prompt').height();
+    };
+
+    function computeChatAreaHeight() {
+        return $('#chat').height() - $('#chat > .current-title').height();
+    };
+
+    function setHeight() {
+        $('#chat-wrapper').height(computeChatWrapperHeight());
+
+        // TODO: find a layout that permit to set the height of .chat-area in css
+        $('#chat > .server, #chat > .chat-area').height(computeChatAreaHeight());
+    };
+
+    $(window).resize(function () {
+        setHeight();
+    });
+
+    setHeight();
+
 });
